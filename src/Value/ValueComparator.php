2350df0c846f41108a189365567977aec78bb522
allflame <le@mirohost.net>
2016-05-10 10:06:16 +0300

    Fixed package names

---
+++ src/Value/ValueComparator.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allflame
+ * Date: 5/10/16
+ * Time: 10:01 AM
+ */
+
+namespace Vain\Comparator\Value;
+
+use Vain\Comparator\AbstractComparator;
+
+class ValueComparator extends AbstractComparator
+{
+    /**
+     * @param ValueObjectInterface $what
+     * @param ValueObjectInterface $against
+     *
+     * @return bool
+     */
+    public function eq($what, $against)
+    {
+        // TODO: Implement eq() method.
+    }
+
+    /**
+     * @param ValueObjectInterface $what
+     * @param ValueObjectInterface $against
+     *
+     * @return bool
+     */
+    public function neq($what, $against)
+    {
+        // TODO: Implement neq() method.
+    }
+
+    /**
+     * @param ValueObjectInterface $what
+     * @param ValueObjectInterface $against
+     *
+     * @return bool
+     */
+    public function lt($what, $against)
+    {
+        // TODO: Implement lt() method.
+    }
+
+    /**
+     * @param ValueObjectInterface $what
+     * @param ValueObjectInterface $against
+     *
+     * @return bool
+     */
+    public function lte($what, $against)
+    {
+        // TODO: Implement lte() method.
+    }
+
+    /**
+     * @param ValueObjectInterface $what
+     * @param ValueObjectInterface $against
+     *
+     * @return bool
+     */
+    public function gt($what, $against)
+    {
+        // TODO: Implement gt() method.
+    }
+
+    /**
+     * @param ValueObjectInterface $what
+     * @param ValueObjectInterface $against
+     *
+     * @return bool
+     */
+    public function gte($what, $against)
+    {
+        // TODO: Implement gte() method.
+    }
+
+    /**
+     * @param ValueObjectInterface $what
+     * @param ValueObjectInterface $against
+     *
+     * @return bool
+     */
+    public function in($what, $against)
+    {
+        // TODO: Implement in() method.
+    }
+
+    /**
+     * @param ValueObjectInterface $what
+     * @param ValueObjectInterface $against
+     *
+     * @return bool
+     */
+    public function like($what, $against)
+    {
+        // TODO: Implement like() method.
+    }
+}
