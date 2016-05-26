122475d2a78293401a2c81d1175e65d1a7befa7f
allflame <taras.p.gyrnik@gmail.com>
2016-05-26 11:40:42 +0300

    Added comparable result

---
+++ src/Result/ComparableResult.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allflame
+ * Date: 5/26/16
+ * Time: 11:36 AM
+ */
+
+namespace Vain\Comparator\Result;
+
+use Vain\Core\Result\AbstractResult;
+
+class ComparableResult extends AbstractResult implements ComparableResultInterface
+{
+    private $expected;
+
+    private $actual;
+
+    private $difference;
+
+    /**
+     * ComparableResult constructor.
+     * @param bool $status
+     * @param mixed $expected
+     * @param mixed $actual
+     * @param mixed $difference
+     */
+    public function __construct($status, $expected, $actual, $difference)
+    {
+        $this->expected = $expected;
+        $this->actual = $actual;
+        $this->difference = $difference;
+        parent::__construct($status);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getExpected()
+    {
+        return $this->expected;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getActual()
+    {
+        return $this->actual;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getDifference()
+    {
+        return $this->difference;
+    }
+}
---
+++ src/Result/ComparableResultInterface.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allflame
+ * Date: 5/26/16
+ * Time: 11:33 AM
+ */
+
+namespace Vain\Comparator\Result;
+
+use Vain\Core\Result\ResultInterface;
+
+interface ComparableResultInterface extends ResultInterface
+{
+    /**
+     * @return mixed
+     */
+    public function getExpected();
+
+    /**
+     * @return mixed
+     */
+    public function getActual();
+
+    /**
+     * @return mixed
+     */
+    public function getDifference();
+}
