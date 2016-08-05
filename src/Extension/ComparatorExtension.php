4edc24434e5bb89b9337dd48a27c54ae9d612741
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-08-05 12:19:15 +0300

    Added di.yml

---
+++ src/Extension/ComparatorExtension.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-comparator
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-comparator
+ */
+namespace Vain\Comparator\Extension;
+
+use Vain\Core\Extension\AbstractExtension;
+
+/**
+ * Class ComparatorExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class ComparatorExtension extends AbstractExtension
+{
+}
